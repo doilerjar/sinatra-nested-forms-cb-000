b25e3b84a6dfe6391931dc085d9286828d71dd6d
Kevin K <doilerjar@outlook.com>
2019-10-24 17:58:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require './environment'
+require_relative 'models/pirate.rb'
+require_relative 'models/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do
+      erb :new
+    end
+    
+    post '/show' do
+      @params = params
+      
+      erb :show
+    end
+    
   end
 end
